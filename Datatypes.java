6eb34126b7001185b4bd84260e06c52844e2f45e
Virat Srivastava <98209563+ViratSrivastava@users.noreply.github.com>
2023-06-28 20:07:27 +0530

    Add files via upload

---
+++ Datatypes.java
@@ -0,0 +1,34 @@
+package Java.DSA;
+class Datatypes
+{
+    public static void main(String arg[])
+    {
+        System.out.println("Int min Value:"+Integer.MIN_VALUE);
+        System.out.println("Int max Value:"+Integer.MAX_VALUE);
+        System.out.println("Int Byte size:"+Integer.BYTES);
+
+        System.out.println("Byte min Value:"+Byte.MIN_VALUE);
+        System.out.println("Byte max Value:"+Byte.MAX_VALUE);
+        System.out.println("Byte size:"+Byte.BYTES);
+
+        System.out.println("Float min Value:"+Float.MIN_VALUE);
+        System.out.println("Float max Value:"+Float.MAX_VALUE);
+        System.out.println("Float Byte size:"+Float.BYTES);
+        
+        System.out.println("Character min Value:"+Character.MIN_VALUE);
+        System.out.println("Character max Value:"+Character.MAX_VALUE);
+        System.out.println("Character Byte size:"+Character.BYTES);
+    }
+}
+//Int min Value:-2147483648
+//Int max Value:2147483647
+//Int Byte size:4
+//Byte min Value:-128
+//Byte max Value:127
+//Byte size:1
+//Float min Value:1.4E-45
+//Float max Value:3.4028235E38
+//Float Byte size:4
+//Character min Value:
+//Character max Value:?
+//Character Byte size:2
